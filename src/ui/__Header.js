4b8c157e25c0633950a7163ec2ac6774d8de3836
Graph and Co <contact@graphandco.com>
2022-03-04 20:19:47 +0100

    Organized folders structure

--- src/ui/__Header.js
+++ src/ui/__Header.js
@@ -1,8 +1,8 @@
-import Logo from './Logo';
-import NavLink from './NavLink';
+import Logo from './__Logo';
+import NavLink from './__NavLink';
 import { useWindowSize } from '../hooks/useWindowSize';
 import { FaHome, FaBoxOpen, FaImages, FaEnvelope } from 'react-icons/fa';
-import Nav from './Nav';
+import Nav from './__Nav';
 
 const Header = () => {
     const responsive = useWindowSize();
