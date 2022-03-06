96c4e8b2230a7fee446cbbd245978038fca7c8c9
Graph and Co <contact@graphandco.com>
2022-03-06 14:22:47 +0100

    Fix deploy issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const withMDX = require('@next/mdx')({
-    extension: /\.mdx?$/,
-});
-module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-    // experimental: {
-    //     // Enables the styled-components SWC transform
-    //     styledComponents: true,
-    // },
-});
+// const withMDX = require('@next/mdx')({
+//     extension: /\.mdx?$/,
+// });
+// module.exports = withMDX({
+//     pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+//     // experimental: {
+//     //     // Enables the styled-components SWC transform
+//     //     styledComponents: true,
+//     // },
+// });
