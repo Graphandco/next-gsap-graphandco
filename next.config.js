164b3f59fbe6acf99b5606894dafb8c931d485e7
Graph and Co <contact@graphandco.com>
2022-03-06 14:44:25 +0100

    Fix build issue

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-// const withMDX = require('@next/mdx')({
-//     extension: /\.mdx?$/,
-// });
-// module.exports = withMDX({
-//     pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-//     // experimental: {
-//     //     // Enables the styled-components SWC transform
-//     //     styledComponents: true,
-//     // },
-// });
+module.exports = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+};
