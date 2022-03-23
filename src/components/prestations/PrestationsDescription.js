b51dcbc5f85e1e476ede2ccd986fd9c134255640
Graph and Co <contact@graphandco.com>
2022-03-23 20:03:21 +0100

    Prestations and adjust home atouts

--- src/components/prestations/PrestationsDescription.js
+++ src/components/prestations/PrestationsDescription.js
@@ -1,11 +1,15 @@
 import { useEffect } from 'react';
+import { useWindowSize } from '../../hooks/useWindowSize';
 import Link from 'next/link';
+import PrestationsSVGMobile from './PrestationsSVGMobile';
 import PrestationsSVG from './PrestationsSVG';
 import { gsap } from 'gsap/dist/gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 const PrestationsDescription = () => {
+    const responsive = useWindowSize();
+
     useEffect(() => {
         gsap.from('#main_droite', {
             x: -700,
@@ -29,9 +33,7 @@ const PrestationsDescription = () => {
         <section className="prestations-description">
             <div className="prestations-description-wrapper container">
                 <h2>Nous réalisons le site taillé pour vous !</h2>
-                <div className="svg-wrapper">
-                    <PrestationsSVG />
-                </div>
+                <div className="svg-wrapper">{responsive === 'mobile' ? <PrestationsSVGMobile /> : <PrestationsSVG />}</div>
                 <div className="prestations-top-content">
                     <p>
                         Nous avons à coeur de définir avec vous le site qui <strong>vous ressemble</strong>. En fonction de vos besoins nous établirons ensemble
