a5fa719d9d91b079d114020c634536cc5d5a7626
Graph and Co <contact@graphandco.com>
2022-04-17 17:55:15 +0200

    Add google Analytics

--- src/layout/Wrapper.js
+++ src/layout/Wrapper.js
@@ -1,14 +1,15 @@
 import { ThemeProvider } from 'theme-ui';
 import Layout from './Layout';
 import Theme from './Theme';
+import ReactGA from 'react-ga';
 
 const Wrapper = (props) => {
     // Want Google Analytics? --> https://github.com/react-ga/react-ga --> add your UI code below
-    // ReactGA.initialize("UA-1234567-89")
-    // ReactGA.set({ anonymizeIp: true })
-    // if (typeof window !== "undefined") {
-    //   ReactGA.pageview(window.location.pathname + window.location.search)
-    // }
+    ReactGA.initialize('UA-154610382-4');
+    ReactGA.set({ anonymizeIp: true });
+    if (typeof window !== 'undefined') {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }
 
     return (
         <ThemeProvider theme={Theme}>
